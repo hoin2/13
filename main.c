e29f9491687a98eca7318111760e50d4b6385e6c
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-30 10:01:01 +0900

    01-2

--- main.c
+++ main.c
@@ -11,10 +11,15 @@ struct student{
 };
 
 int main(void) {
-	struct student student1;
+	struct student student1 = {2211785, "SeongHojin", 3.9};
 	
 	student1.ID = 2211753;
 	strcpy(student1.name, "HojinSeong");
 	student1.grade = 4.1;
+	
+	printf("ID : %i\n",student1.ID);
+	printf("name : %s\n",student1.name);
+	printf("grade : %.1f\n",student1.grade);
+	
 	return 0;
 }
